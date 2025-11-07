4e35d2a53236c78011664a5c3a912708f056c021
baxeezy <danya.bahv@yandex.ru>
2025-11-07 23:45:09 +0300

    Добавление теста для темной темы

--- TrackerTests/TrackerTests.swift
+++ TrackerTests/TrackerTests.swift
@@ -4,7 +4,7 @@ import SnapshotTesting
 
 final class TrackerTests: XCTestCase {
     
-    func testOnboardingViewController() {
+    func testTrackerViewControllerLightTheme() {
         // Given
         let vc = TrackerViewController()
         
@@ -12,6 +12,25 @@ final class TrackerTests: XCTestCase {
         vc.loadViewIfNeeded()
         
         // Then
-        assertSnapshot(of: vc, as: .image)
+        assertSnapshot(
+            of: vc,
+            as: .image(traits: .init(userInterfaceStyle: .light)),
+            named: "LightTheme"
+        )
+    }
+    
+    func testTrackerViewControllerDarkTheme() {
+        // Given
+        let vc = TrackerViewController()
+        
+        // When
+        vc.loadViewIfNeeded()
+        
+        // Then
+        assertSnapshot(
+            of: vc,
+            as: .image(traits: .init(userInterfaceStyle: .dark)),
+            named: "DarkTheme"
+        )
     }
 }
